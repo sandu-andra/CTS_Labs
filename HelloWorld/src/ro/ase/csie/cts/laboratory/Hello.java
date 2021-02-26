082898e0ec7979c1b52079f8bc8ce2da35e9c7ad
Andra Sandu <andraa.sandu.99@gmail.com>
2021-02-26 15:12:22 +0200

    Hello World commit

--- HelloWorld/src/ro/ase/csie/cts/laboratory/Hello.java
+++ HelloWorld/src/ro/ase/csie/cts/laboratory/Hello.java
@@ -3,8 +3,9 @@ package ro.ase.csie.cts.laboratory;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("Hello world!"); //Syso ctrl space
+       
 	}
 
 }
