b34c3da0571e93063da758795978ce4b31d87e74
Andra Sandu <andraa.sandu.99@gmail.com>
2021-03-04 11:29:13 +0200

    Course2 commit

---
+++ Course2/src/ro/ase/csie/cts/course2/models/Faculty.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.course2.models;
+
+public class Faculty {
+	String faculty;
+	int groupNo;
+	
+}
---
+++ Course2/src/ro/ase/csie/cts/course2/models/Student.java
@@ -0,0 +1,33 @@
+package ro.ase.csie.cts.course2.models;
+
+public class Student {
+		
+	    PersonalData personalData;
+		Faculty faculty;
+		
+		int[] grades;
+		
+		public int getMinGrade() {
+			int minimum = this.grades[0];
+			for(int grade : this.grades) {
+				if(minimum > grade) {
+					minimum = grade;
+				}
+			}
+			return minimum;		
+	    }		
+
+		public int getNoMinGrades() {
+	
+		   int minimum = this.getMinGrade();
+			
+			int noGrades = 0;
+			for(int grade : this.grades) {
+				if(grade == minimum) {
+					noGrades += 1;
+				}
+			}
+			return noGrades;
+	    }
+	
+}
