3121c858db7255d82911b81fa2c244a05264c641
Andra Sandu <andraa.sandu.99@gmail.com>
2021-03-11 12:42:34 +0200

    Commit

---
+++ Course3/src/ro/ase/csie/cts/course3/farmework/TestLiskov.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.course3.farmework;
+
+public class TestLiskov {
+	
+	public static void walk(ActionsInterface entity) {
+		entity.walk();
+	}
+
+	public static void main(String[] args) {
+		
+		//for Persons
+		Person person = new Person();
+		walk(person);
+		
+		//for Students
+		Student student = new Student();
+		walk(student);
+		
+	}
+}
