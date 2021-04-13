7ef819ccdb28084a9dd023eec60ea428a91e87fd
Sandu Andra <andraa.sandu.99@gmail.com>
2021-04-13 14:33:56 +0300

    Facade implementation, Adapter started

---
+++ Week8 - Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/adapter/disney/DisneyActions.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1094.dp.adapter.disney;
+
+public interface DisneyActions {
+
+	public void changeLocation(int x, int y);
+	public void isWounded(int power);
+	public void restorePower(int power);
+}
---
+++ Week8 - Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/facade/UserAccount.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1094.dp.facade;
+
+public class UserAccount {
+
+	public void login(String username, String pass) {
+		System.out.println("Checking credentials...");
+	}
+	
+	public UserProfile downloadUserProfile() {
+		return new UserProfile();
+	}
+}
