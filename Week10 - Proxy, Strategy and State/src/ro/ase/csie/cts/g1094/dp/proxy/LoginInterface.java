b69bec3991ffbca75e2f212d4aece86ac2a45c12
Sandu Andra <andraa.sandu.99@gmail.com>
2021-05-06 14:33:19 +0300

    Implementing Proxy

---
+++ Week10 - Proxy, Strategy and State/src/ro/ase/csie/cts/g1094/dp/proxy/LoginInterface.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1094.dp.proxy;
+
+public interface LoginInterface {
+
+	public boolean login(String username, String pass);
+	public boolean checkServerStatus();
+	
+}
---
+++ Week10 - Proxy, Strategy and State/src/ro/ase/csie/cts/g1094/dp/proxy/LoginModule.java
@@ -0,0 +1,27 @@
+package ro.ase.csie.cts.g1094.dp.proxy;
+
+public class LoginModule implements LoginInterface{
+
+	String serverIP;
+	String dbConnection;
+	
+	public LoginModule(String serverIP, String dbConnection) {
+		super();
+		this.serverIP = serverIP;
+		this.dbConnection = dbConnection;
+	}
+	
+	@Override
+	public boolean login(String username, String pass) {
+		System.out.println("Connection to the database: " + dbConnection);
+	    if(username.toLowerCase().equals("admin") && pass.equals("1234admin"))
+	    	return true;
+	    else
+	    	return false;
+	}
+	@Override
+	public boolean checkServerStatus() {
+         return true;
+	}
+	
+}
