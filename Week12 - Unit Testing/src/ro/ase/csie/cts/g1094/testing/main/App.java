e530f22965004a8c2d4097ea9432ac418c50a506
Sandu Andra <andraa.sandu.99@gmail.com>
2021-05-18 15:32:24 +0300

    Implementing a TestCase in JUnit

---
+++ Week12 - Unit Testing/src/ro/ase/csie/cts/g1094/testing/main/App.java
@@ -0,0 +1,24 @@
+package ro.ase.csie.cts.g1094.testing.main;
+
+import ro.ase.csie.cts.g1094.testing.models.MathOperations;
+
+public class App {
+	
+   public static void main(String[] args) {
+	
+	   System.out.println("App started");
+	   
+//	   int a = 5;
+//	   int b = 6;
+//	   int expectedResult = 11;
+//	   int actualResult = MathOperations.add(a, b);
+//	   if(actualResult == expectedResult) {
+//		   System.out.println("I know how to add");
+//	   }
+//	   else 
+//	   {
+//		   System.out.println("Let's get some math classes");
+//	   }
+		   
+   }
+}
